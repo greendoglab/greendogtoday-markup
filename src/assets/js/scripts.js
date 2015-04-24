123749ceff9879a2043025a415e5cba687e6e93c
Michael Yakimenko <macgera@gmail.com>
2015-04-25 00:28:09 +0800

    Mobile menu & fixes.

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,7 +1,28 @@
 (function($) {
 
+    function stateCheck(elem, clName) {
+        if (!elem.hasClass(clName)) {
+            elem.addClass(clName);
+        } else {
+            elem.removeClass(clName);
+        }
+    }
+
+    function mobileMenu(){
+        var mobileMenuTrigger = $('[data-role="mobile-header-trigger"]');
+        var mobileHeader = $('[data-role="mobile-header"]');
+
+        mobileMenuTrigger.click(function() {
+            $(this).toggleClass('active');
+            stateCheck(mobileHeader, 'active');
+            console.log('f')
+        });
+    }
+
     // document ready
-    $(window).on('load', function() {});
+    $(window).on('load', function() {
+        mobileMenu();
+    });
 
     // all initial on window resize
     $(window).on('resize', function() {});
