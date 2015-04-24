9689b8b21300878cb8ff59a34179e7a005712770
Michael Yakimenko <macgera@gmail.com>
2015-04-25 00:31:12 +0800

    Image portrait jquery script.

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -19,9 +19,23 @@
         });
     }
 
+    function imagePortrait() {
+        var sectionImages = $('img', 'section.post');
+
+        sectionImages.each(function() {
+            var image = $(this);
+            console.log(image);
+            if (image.width() < image.height()) {
+                console.log('portrait');
+                image.addClass('portrait');
+            }
+        });
+    }
+
     // document ready
     $(window).on('load', function() {
         mobileMenu();
+        imagePortrait();
     });
 
     // all initial on window resize
